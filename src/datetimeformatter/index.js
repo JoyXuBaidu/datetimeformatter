b78c103f2760883a812110fe5b6138a8ed142289
JoyXuBaidu <773959647@qq.com>
2021-05-18 10:16:44 +0800

    init version 0.1

---
+++ src/datetimeformatter/index.js
@@ -0,0 +1,73 @@
+let _Vue = null
+
+const DateTimeFormatterPlugin = {
+  install(Vue) {
+    _Vue = Vue
+
+    const that = this
+    _Vue.mixin({
+      filters: {
+        dateTimeFilter(val, format) {
+          const date = that.initDate(val)
+          return that.formatOutput(date, format)
+        }
+      },
+      methods: {
+        dateTimeFormatter(attr, format) {
+          return (row) => {
+            const date = that.initDate(row[attr])
+            return that.formatOutput(date, format)
+          }
+        },
+        dateTimeRangeFormatter(startAttr, endAttr, format, link) {
+          return (row) => {
+            const startDate = that.initDate(row[startAttr])
+            const endDate = that.initDate(row[endAttr])
+            console.log(that.formatOutput(startDate, format))
+            return that.formatOutput(startDate, format) + (link ? link : '~') + that.formatOutput(endDate, format)
+          }
+        }
+      }
+    })
+  },
+
+  padStart(str) {
+    return String(str).length < 2 ? String(str).padStart(2, '0') : String(str)
+  },
+
+  initDate(val) {
+    return new Date(val.substring(0, 4), Number(val.substring(4, 6) - 1), val.substring(6, 8), val.substring(8, 10), val.substring(10, 12), val.substring(12, 14))
+  },
+
+  formatOutput(date, format) {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+
+    return format.replace(/Y{2,4}|M{1,2}|D{1,2}|h{1,2}|H{1,2}|m{1,2}|s{1,2}/g,
+      match => {
+        switch (match) {
+          case 'YY': return String(year).slice(-2)
+          case 'YYYY': return String(year)
+          case 'M': return String(month)
+          case 'MM': return this.padStart(month)
+          case 'D': return String(day)
+          case 'DD': return this.padStart(day)
+          case 'h': return String(hour)
+          case 'hh': return this.padStart(hour)
+          case 'H': return String(hour)
+          case 'HH': return this.padStart(hour)
+          case 'm': return String(minute)
+          case 'mm': return this.padStart(minute)
+          case 's': return String(second)
+          case 'ss': return this.padStart(second)
+          default: return String(match)
+        }
+      })
+  }
+}
+
+export default DateTimeFormatterPlugin
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/datetimeformatter/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'formatter.js',
+    library: 'datetimeFormatter', // library指定的就是你使用require时的模块名，这里便是require("datetimeFormatter")
+    libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+}
